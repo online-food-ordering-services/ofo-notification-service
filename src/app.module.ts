44d231685f1a9f8a17ca55d09c5a1d97d312e0b6
phong-do-tpv-mti <phong.do.tpv@one-line.com>
2025-06-15 14:26:26 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
